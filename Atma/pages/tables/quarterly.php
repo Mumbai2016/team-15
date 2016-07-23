a5136681fbd6388cfda60b36daf8d045f31a6e8c
sanjay <sanjay.lulla@ves.ac.in>
2016-07-24 04:46:15 +0530

    quarterly.php with Form feeds for assign tasks SP,php color change

--- Atma/pages/tables/quarterly.php
+++ Atma/pages/tables/quarterly.php
@@ -389,102 +389,6 @@
                                 <li><a href="../UI/modals.html"><i class="fa fa-circle-o"></i> Modals</a></li>
                             </ul>
                         </li>
-                        <li class="treeview">
-                            <a href="#">
-                            <i class="fa fa-edit"></i> <span>Forms</span>
-                            <span class="pull-right-container">
-                            <i class="fa fa-angle-left pull-right"></i>
-                            </span>
-                            </a>
-                            <ul class="treeview-menu">
-                                <li><a href="../forms/general.html"><i class="fa fa-circle-o"></i> General Elements</a></li>
-                                <li><a href="../forms/advanced.html"><i class="fa fa-circle-o"></i> Advanced Elements</a></li>
-                                <li><a href="../forms/editors.html"><i class="fa fa-circle-o"></i> Editors</a></li>
-                            </ul>
-                        </li>
-                        <li class="treeview active">
-                            <a href="#">
-                            <i class="fa fa-table"></i> <span>Tables</span>
-                            <span class="pull-right-container">
-                            <i class="fa fa-angle-left pull-right"></i>
-                            </span>
-                            </a>
-                            <ul class="treeview-menu">
-                                <li class="active"><a href="simple.html"><i class="fa fa-circle-o"></i> Simple tables</a></li>
-                                <li><a href="data.html"><i class="fa fa-circle-o"></i> Data tables</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="../calendar.html">
-                            <i class="fa fa-calendar"></i> <span>Calendar</span>
-                            <span class="pull-right-container">
-                            <small class="label pull-right bg-red">3</small>
-                            <small class="label pull-right bg-blue">17</small>
-                            </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="../mailbox/mailbox.html">
-                            <i class="fa fa-envelope"></i> <span>Mailbox</span>
-                            <span class="pull-right-container">
-                            <small class="label pull-right bg-yellow">12</small>
-                            <small class="label pull-right bg-green">16</small>
-                            <small class="label pull-right bg-red">5</small>
-                            </span>
-                            </a>
-                        </li>
-                        <li class="treeview">
-                            <a href="#">
-                            <i class="fa fa-folder"></i> <span>Examples</span>
-                            <span class="pull-right-container">
-                            <i class="fa fa-angle-left pull-right"></i>
-                            </span>
-                            </a>
-                            <ul class="treeview-menu">
-                                <li><a href="../examples/invoice.html"><i class="fa fa-circle-o"></i> Invoice</a></li>
-                                <li><a href="../examples/profile.html"><i class="fa fa-circle-o"></i> Profile</a></li>
-                                <li><a href="../examples/login.html"><i class="fa fa-circle-o"></i> Login</a></li>
-                                <li><a href="../examples/register.html"><i class="fa fa-circle-o"></i> Register</a></li>
-                                <li><a href="../examples/lockscreen.html"><i class="fa fa-circle-o"></i> Lockscreen</a></li>
-                                <li><a href="../examples/404.html"><i class="fa fa-circle-o"></i> 404 Error</a></li>
-                                <li><a href="../examples/500.html"><i class="fa fa-circle-o"></i> 500 Error</a></li>
-                                <li><a href="../examples/blank.html"><i class="fa fa-circle-o"></i> Blank Page</a></li>
-                                <li><a href="../examples/pace.html"><i class="fa fa-circle-o"></i> Pace Page</a></li>
-                            </ul>
-                        </li>
-                        <li class="treeview">
-                            <a href="#">
-                            <i class="fa fa-share"></i> <span>Multilevel</span>
-                            <span class="pull-right-container">
-                            <i class="fa fa-angle-left pull-right"></i>
-                            </span>
-                            </a>
-                            <ul class="treeview-menu">
-                                <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-                                <li>
-                                    <a href="#"><i class="fa fa-circle-o"></i> Level One
-                                    <span class="pull-right-container">
-                                    <i class="fa fa-angle-left pull-right"></i>
-                                    </span>
-                                    </a>
-                                    <ul class="treeview-menu">
-                                        <li><a href="#"><i class="fa fa-circle-o"></i> Level Two</a></li>
-                                        <li>
-                                            <a href="#"><i class="fa fa-circle-o"></i> Level Two
-                                            <span class="pull-right-container">
-                                            <i class="fa fa-angle-left pull-right"></i>
-                                            </span>
-                                            </a>
-                                            <ul class="treeview-menu">
-                                                <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
-                                                <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-                            </ul>
-                        </li>
                         <li><a href="../../documentation/index.html"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
                         <li class="header">LABELS</li>
                         <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
@@ -648,6 +552,34 @@
                             <!-- /.box -->
                         </div>
                     </div>
+					<hr>
+					<div class="box-body table-responsive no-padding">
+                                    <table class="table table-hover">
+                                
+					<tr>
+                                            <th>AIP ID</th>
+                                            <th>Task ID</th>
+                                            <th>Task Description</th>
+                                            <th>Start Date</th>
+                                            <th>End Date</th>
+                                            <th>Quarter</th>
+                                            <th>Volunteer</th>
+                                            <th>Status</th>
+                                            <th>Resources</th>
+                                        </tr>
+										<tr>
+                                              <td><input type="text" class="form-control"></td>
+                                              <td><input type="text" class="form-control"></td>
+                                              <td><input type="text" class="form-control"></td>
+                                              <td><input type="text" class="form-control"></td>
+                                              <td><input type="text" class="form-control"></td>
+                                            <td><input type="text" class="form-control"></td>
+                                            <td><input type="text" class="form-control"></td>
+                                            <td><input type="text" class="form-control"></td>
+                                            <td><input type="text" class="form-control"></td>
+                                            </tr>
+									</table>
+									</div>
 					<div class="btn btn-primary pull-right">Assign new tasks</div>
                 </section>
                 <!-- /.content -->
