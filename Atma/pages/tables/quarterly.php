b0da5e946b4dce3fd8ecd615217876c4fbb5652d
sanjay <sanjay.lulla@ves.ac.in>
2016-07-24 01:02:36 +0530

    Table Headings inserted and formatted

--- Atma/pages/tables/quarterly.php
+++ Atma/pages/tables/quarterly.php
@@ -514,7 +514,7 @@
         <div class="col-xs-12">
           <div class="box">
             <div class="box-header">
-              <h3 class="box-title">Responsive Hover Table</h3>
+              <h3 class="box-title">Quarterly Table</h3>
 
               <div class="box-tools">
                 <div class="input-group input-group-sm" style="width: 150px;">
@@ -530,11 +530,15 @@
             <div class="box-body table-responsive no-padding">
               <table class="table table-hover">
                 <tr>
-                  <th>ID</th>
-                  <th>User</th>
-                  <th>Date</th>
-                  <th>Status</th>
-                  <th>Reason</th>
+                  <th>AIP ID</th>
+                  <th>Task ID</th>
+                  <th>Task Description</th>
+                  <th>Start Date</th>
+                  <th>End Date</th>
+				  <th>Quarter</th>
+				  <th>Volunteer</th>
+				  <th>Status</th>
+				  <th>Resources</th>
                 </tr>
                 <tr>
                   <td>183</td>
