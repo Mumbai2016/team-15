1bfd692830a582d1c5b72209497429ad0654f7cf
Kashyap Chetan Kotak <kashyap.kotak@ves.ac.in>
2016-07-23 22:08:11 +0530

    login atma.php created now a pm will transfer to pmlogin rest pages not defined(currently going to PMlogin.html)

--- Atma/login atma.php
+++ Atma/login atma.php
@@ -27,11 +27,11 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+    <a href="../../index2.html"><b>WELCOME TO ATMA LOGIN</b></a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">Sign in to start your session</p>
+    <p align="left" class="login-box-msg">Login as Volunteer</p>
 
     <form action="../../index2.html" method="post">
       <div class="form-group has-feedback">
@@ -43,13 +43,7 @@
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> Remember Me
-            </label>
-          </div>
-        </div>
+        
         <!-- /.col -->
         <div class="col-xs-4">
           <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
@@ -58,17 +52,7 @@
       </div>
     </form>
 
-    <div class="social-auth-links text-center">
-      <p>- OR -</p>
-      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-        Facebook</a>
-      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-        Google+</a>
-    </div>
-    <!-- /.social-auth-links -->
-
-    <a href="#">I forgot my password</a><br>
-    <a href="register.html" class="text-center">Register a new membership</a>
+   
 
   </div>
   <!-- /.login-box-body -->
