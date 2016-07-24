e4ba7212c09ed6a23b2759c645b31771bf18908e
stg4040 <stg.4040@gmail.com>
2016-07-24 06:51:37 +0530

    Database connectivity credentials update

--- Atma/dbconnect.php
+++ Atma/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 $db_host = "localhost";
 $db_username = "root";
-$db_password = "";
+$db_password = "root";
 $db_name =  "atma";
 $conn = mysql_connect("$db_host", "$db_username", "$db_password") or die("Could not connect to MYSQL");
 @mysql_select_db("$db_name") or die("Could not connect to database schema");
